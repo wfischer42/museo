0e2e757fdda885a542b98bdb556be0e39e8ccc57
William Fischer <william.fischer42@gmail.com>
2018-09-18 09:28:38 -0600

    Added iniitializer and attr_readers to curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,3 +1,9 @@
 class Curator
-  
+  attr_reader :artists,
+              :photographs
+
+  def initialize
+    @artists = []
+    @photographs = []
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -5,7 +5,13 @@ class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
   end
+
   def test_it_exists
     assert_instance_of Curator, @curator
+  end
+
+  def test_it_has_no_artists_or_photographs_by_default
+    assert_equal [], @curator.artists
+    assert_equal [], @curator.photographs
   end
 end
